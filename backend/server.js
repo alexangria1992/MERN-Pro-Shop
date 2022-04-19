1b33580a413bbb6c03265ac61365563bd1c5a15d
alexangria1992 <alex040892@gmail.com>
2022-04-19 23:18:01 +1200

    fetching products from database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import colors from "colors";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/products", productRoutes);
 
 app.get("/api/products/", (req, res) => {
   res.json(products);
